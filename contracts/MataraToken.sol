dc71d0fec9e5c2cd2ed1f2a7d3b2c45771027dd8
yasindu <yazindu@gmail.com>
2024-03-24 23:08:59 +0530

    feat: Add owner

--- contracts/MataraToken.sol
+++ contracts/MataraToken.sol
@@ -1,10 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
+
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract MataraToken is ERC20{
+contract MataraToken is ERC20 {
+    address payable public owner;
     constructor(uint256 initialSupply) ERC20("MataraToken", "MTR"){
-        _mint(msg.sender, initialSupply);
+        owner = msg.sender;
+        _mint(owner, initialSupply);
     }
 
 }
