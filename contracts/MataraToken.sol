c2e429f72bb62f6bd08822ff24ec95ceb069d6d4
yasindu <yazindu@gmail.com>
2024-03-24 23:18:00 +0530

    feat: Fix initial supply and apply Cap

--- contracts/MataraToken.sol
+++ contracts/MataraToken.sol
@@ -2,12 +2,13 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 
-contract MataraToken is ERC20 {
+contract MataraToken is ERC20Capped {
     address payable public owner;
-    constructor(uint256 initialSupply) ERC20("MataraToken", "MTR"){
+    constructor(uint256 cap) ERC20("MataraToken", "MTR") ERC20Capped(cap * (10 ** decimals())){
         owner = msg.sender;
-        _mint(owner, initialSupply);
+        _mint(owner, 70000000 * (10 ** decimals()));
     }
 
 }
