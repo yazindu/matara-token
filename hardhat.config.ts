c2da29954463f81adb645e83605980be5ce295d9
yasindu <yazindu@gmail.com>
2024-03-28 07:05:08 +0530

    wip: deploy contract to sepolia testnet

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,15 @@
-import { HardhatUserConfig } from "hardhat/config";
+import {HardhatUserConfig} from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import 'dotenv/config'
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.9",
+    solidity: "0.8.9",
+    networks: {
+        sepolia:{
+          url: process.env.SEPOLIA_RPC_URL,
+          accounts: [process.env.PRIVATE_KEY as string]
+        }
+    }
 };
 
 export default config;
