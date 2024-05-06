40f5caeaaf3d19e08dcadff33d093419e16468bc
yasindu <yazindu@gmail.com>
2024-05-06 22:55:50 +0530

    feat: installed solidity-coverage

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,7 @@
 import {HardhatUserConfig} from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import 'dotenv/config'
+import 'solidity-coverage'
 
 const config: HardhatUserConfig = {
     solidity: "0.8.9",
